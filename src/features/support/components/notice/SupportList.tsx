655130986f72bc520915a2dc90a64ec9e1f8ce65
jun <pickjog@gmail.com>
2025-02-07 12:15:36 +0900

    update NoticeList Data

--- src/features/support/components/notice/SupportList.tsx
+++ src/features/support/components/notice/SupportList.tsx
@@ -23,7 +23,7 @@ const Category = styled.strong`
   font-size: 14px;
   letter-spacing: -0.21px;
   min-width: 68px;
-  width: 68px;
+  width: 120px;
 `;
 
 const TitleBox = styled.div`
@@ -40,7 +40,13 @@ const Title = styled.p`
 interface SupportListProps {
   notices: { id: number; category: string; title: string }[]; // 공지 데이터 배열
 }
-
+// 1) "영어 → 한글" 매핑 객체
+const noticeCategoryToKorean: Record<string, string> = {
+  ANNOUNCEMENT: "공지",
+  EVENT: "이벤트",
+  SERVICE: "서비스 안내",
+  OTHERS: "기타",
+};
 const SupportList: React.FC<SupportListProps> = ({ notices }) => {
   const navigate = useNavigate();
 
@@ -49,14 +55,20 @@ const SupportList: React.FC<SupportListProps> = ({ notices }) => {
   };
   return (
     <ListContainer>
-      {notices.map((notice) => (
-        <li key={notice.id} onClick={() => handleClick(notice.id)}>
-          <Category>{notice.category}</Category>
-          <TitleBox>
-            <Title>{notice.title}</Title>
-          </TitleBox>
-        </li>
-      ))}
+      {notices.map((notice) => {
+        // 2) 영어 category → 한글 category
+        const koreanCategory =
+          noticeCategoryToKorean[notice.category] || notice.category;
+
+        return (
+          <li key={notice.id} onClick={() => handleClick(notice.id)}>
+            <Category>{koreanCategory}</Category>
+            <TitleBox>
+              <Title>{notice.title}</Title>
+            </TitleBox>
+          </li>
+        );
+      })}
     </ListContainer>
   );
 };
