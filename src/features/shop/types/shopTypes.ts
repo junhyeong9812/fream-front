f28ad7e564e6b2b3b146cf677b3a9158e7bda360
jun <pickjog@gmail.com>
2025-02-10 20:56:09 +0900

    shop Update

--- src/features/shop/types/shopTypes.ts
+++ src/features/shop/types/shopTypes.ts
@@ -1,5 +1,14 @@
-export interface ShopItem {
-  id: string;
+export interface ImageData {
+  id: number;
   name: string;
+  englishName: string;
+  brandName: string;
+  releasePrice: number;
+  thumbnailImageUrl: string;
   price: number;
+  colorName: string;
+  colorId: number;
+  interestCount: number;
+  styleCount: number;
+  tradeCount: number;
 }
