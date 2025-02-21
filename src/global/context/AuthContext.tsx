ab91a1d4df28da37a3d2232ba116df72be6f526a
jun <pickjog@gmail.com>
2025-02-21 16:02:57 +0900

    fixed Login

--- src/global/context/AuthContext.tsx
+++ src/global/context/AuthContext.tsx
@@ -15,6 +15,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true); // 초기 로딩 상태 추가
 
   useEffect(() => {
     // 컴포넌트 마운트 시점에 로컬스토리지를 확인
@@ -38,10 +39,12 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
         setIsLoggedIn(false);
       }
     }
+    setLoading(false); // 상태 로딩 완료
   }, []);
 
   // (선택) 매 분마다 만료 체크하고 싶다면, setInterval을 둘 수도 있음
   // 다만, 보통은 페이지 리로딩/새로고침 시점에만 검사해도 충분.
+  if (loading) return null; // 데이터가 로드될 때까지 화면 렌더링 방지
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
